1ba50db3827cacb42439c70fd701f3648a0bc1e9
Matthew Kim <madkim@ucsc.edu>
2014-10-21 14:32:22 -0700

    Added filereader with counter to count number of elements in file. Then repeated file read using counter to create an array and read in the contents from file. Finally added a try-catch block to check for number of arguments.

--- BusinessSearch.java
+++ BusinessSearch.java
@@ -10,16 +10,46 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.FileReader;
+import java.util.Scanner;
 
 class BusinessSearch{
 
-  public static void main( String[] args) throws IOException{
-    BufferedReader in = new BufferedReader(
-      new FileReader("favorites.txt"));
-    while(true){
-      String name = in.readLine();
-      if (name == null) break; 
-      System.out.println(name);
-    }
+  public static void main( String[] args){
+    
+    Scanner sc = new Scanner(System.in);
+    int count=0;	//keep track of array length
+  
+  	try{
+  	if(args.length == 1){
+
+  		BufferedReader in = new BufferedReader(
+    		new FileReader("favorites.txt"));
+    	while(true){
+      	String name = in.readLine();
+      	if (name == null) break;  
+      	count++; 
+    	}
+    	//System.out.println(count);	//print count = new array length
+  		String[] businesses = new String[count];
+  		BufferedReader insert = new BufferedReader(
+    		new FileReader("favorites.txt"));
+  		for(int i=0; i<businesses.length; i++){
+  			String name = insert.readLine();
+  			businesses[i] = name;
+  			System.out.println(businesses[i]);
+  		}
+  	}
+
+  	}catch(Exception e){
+  		System.out.println("Usage: BusinessSearch businessDB");
+  		System.exit(1);
+  	}
+  	if(args.length == 0 || args.length >=2){
+  		System.out.println("Usage: BusinessSearch businessDB");
+  		System.exit(1);
+  	}
+
+
+
   }
 }
